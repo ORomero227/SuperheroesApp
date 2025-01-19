aa6eb9a39d2ac804e8fe6233cb0826aa4af962f1
Oscar Romero <oscalito992@gmail.com>
2025-01-18 23:34:52 -0400

    Custom Font Added

--- app/src/main/java/com/example/superheroes/ui/theme/Type.kt
+++ app/src/main/java/com/example/superheroes/ui/theme/Type.kt
@@ -2,18 +2,23 @@ package com.example.superheroes.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.superheroes.R
+
+val Cabin = FontFamily(
+    Font(resId = R.font.cabin_regular),
+    Font(resId = R.font.cabin_bold, weight = FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
